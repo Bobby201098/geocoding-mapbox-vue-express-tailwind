6f6f027bf8ae72a5f46ee9dd6364a3335f3d0d5b
Romi Julianto <romyjulians@gmail.com>
2023-04-08 12:57:01 +0700

    add: refactor code using URLSearchParams

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // initialize server
 const express = require("express");
 const cors = require("cors");
+const url = require("url")
 const { default: axios } = require("axios");
 
 require("dotenv").config();
@@ -14,11 +15,16 @@ app.use(cors());
 // initialize routes
 app.get("/api/search/:query", async (req, res) => {
     try {
+        // refactor code using URLSearchParams
+        const params = new URLSearchParams({
+            access_token: process.env.API_KEY,
+            ...url.parse(req.url, true).query
+
+        })
+        console.log(params)
         const query = req.params.query;
-        const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`)
-        console.log(query)
+        const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`)
         const data = results.data;
-        console.log(data)
         res.status(200).json(data)
     } catch (err) {
         res.status(500).json({ error: err.message });
