af075fb5e045c0babbcdcf19932f40e7524e5382
Romi Julianto <romyjulians@gmail.com>
2023-04-08 12:41:26 +0700

    add: axios implement and result data in json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // initialize server
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 require("dotenv").config();
 
@@ -11,10 +12,14 @@ const app = express();
 app.use(cors());
 
 // initialize routes
-app.get("/api/search/:query", (req, res) => {
+app.get("/api/search/:query", async (req, res) => {
     try {
         const query = req.params.query;
+        const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`)
         console.log(query)
+        const data = results.data;
+        console.log(data)
+        res.status(200).json(data)
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
