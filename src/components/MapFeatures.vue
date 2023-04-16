5f5aa2efb9df147cb066972919c1bbd1fbe80b36
Romi Julianto <romyjulians@gmail.com>
2023-04-16 22:38:39 +0700

    feat: finish function getData from BE

--- src/components/MapFeatures.vue
+++ src/components/MapFeatures.vue
@@ -3,25 +3,29 @@
     class="absolute z-[2] flex w-full gap-4 bg-transparent px-6 py-8 md:left-[60px] md:top-[40px] md:w-auto md:px-0 md:py-0"
   >
     <!-- Search -->
-    <div class="relative flex-1 md:min-w[350px]">
+    <div class="md:min-w[350px] relative flex-1">
       <input
-       class="w-full py-3 pr-4 text-sm rounded-md shadow-md pl-9 focus:outline-none"
+        class="w-full py-3 pr-4 text-sm rounded-md shadow-md pl-9 focus:outline-none"
         type="text"
         placeholder="Search your address"
+        v-model="searchQueryUser"
+        @input="search"
       />
       <!-- Search Icon -->
-      <div class="absolute top-0 left-[8px] h-full flex items-center">
+      <div class="absolute left-[8px] top-0 flex h-full items-center">
         <i class="fas fa-search"></i>
       </div>
 
       <!-- Search Results -->
       <div class="absolute w-full mt-2">
         <!-- Results -->
-        <div class="h-[200px] overflow-scrool bg-white rounded-md">
-            <div class="flex px-4 py-2 cursor-pointer gap-x-2 hover:bg-slate-600 hover:text-white">
-                <i class="fas fa-map-marker-alt"></i>
-                <p class="text-xs">Testing Results</p>
-            </div>
+        <div class="overflow-scrool h-[200px] rounded-md bg-white">
+          <div
+            class="flex px-4 py-2 cursor-pointer gap-x-2 hover:bg-slate-600 hover:text-white"
+          >
+            <i class="fas fa-map-marker-alt"></i>
+            <p class="text-xs">Testing Results</p>
+          </div>
         </div>
       </div>
     </div>
@@ -41,11 +45,11 @@
 </template>
 
 <script>
-import { ref } from 'vue';
-import axios from "axios"
+import { ref } from "vue";
+import axios from "axios";
 
 export default {
-  props: ['coords', 'fetchCoords'],
+  props: ["coords", "fetchCoords"],
   setup(props) {
     const searchQueryUser = ref(null);
     const searchData = ref(null);
@@ -53,20 +57,30 @@ export default {
 
     // functioin searchfromUser
     const search = () => {
-        queryTimeout.value = setTimeout( async() => {
-            if(searchQueryUser.value !== "") {
-                const params = new URLSearchParams({
-                    fuzzyMatch: true,
-                    lang: 'en',
-                    limit: 10,
-                    proximity: props.coords ? `${props.coords.value.lang},${props.coords.value.lat}` : "0,0"
-                });
-                const getData = await axios.get(`http://localhost:3200/api/search/${searchQueryUser.value}?${params}`)
-            }
-        }, 750);
-    }
 
-    return { searchData, searchQueryUser, queryTimeout }
+        // clearTimeOut
+        clearTimeout(queryTimeout.value);
+
+      queryTimeout.value = setTimeout(async () => {
+        if (searchQueryUser.value !== "") {
+          const params = new URLSearchParams({
+            fuzzyMatch: true,
+            lang: "en",
+            limit: 10,
+            proximity: props.coords.value
+              ? `${props.coords.value.lang},${props.coords.value.lat}`
+              : "0,0",
+          });
+          const getData = await axios.get(
+            `http://localhost:3200/api/search/${searchQueryUser.value}?${params}`
+          );
+          searchData.value = getData.data.features;
+          console.log(searchData.value)
+        }
+      }, 750);
+    };
+
+    return { searchData, searchQueryUser, queryTimeout, search };
   },
-}
+};
 </script>
