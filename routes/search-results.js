fcb594f52a9429085eac1b9c315677eb0649827e
Romi Julianto <romyjulians@gmail.com>
2023-04-17 19:35:52 +0700

    change: manipulation result json

--- routes/search-results.js
+++ routes/search-results.js
@@ -13,6 +13,24 @@ router.get("/:query", async (req, res) => {
         })
         const query = req.params.query;
         const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`)
+
+
+        // format data to include city and state
+        results.data.features.forEach((item) => {
+            // init set to null
+            item.city = null;
+            item.state = null;
+
+            // cycle through content results
+            item.context.forEach((type) => {
+                if(type.id.includes("place")) {
+                    item.city = type.text;
+                }
+                if(type.id.includes("region")) {
+                    item.state = type.text
+                }
+            })
+        });
         const data = results.data;
         res.status(200).json(data)
     } catch (err) {
