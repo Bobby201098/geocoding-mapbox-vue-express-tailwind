963535318706d1b9d8e6ba91778ae1b843522aa4
Bobby201098 <44035566+Bobby201098@users.noreply.github.com>
2023-07-02 17:08:59 +0100

    changes to nav

--- src/views/SetariProfil.vue
+++ src/views/SetariProfil.vue
@@ -14,8 +14,9 @@
            </NavbarCollapse>
          </template>
          <template #right-side>
-           <button type="button" @click="login" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log in</button>
-           <button type="button"  @click="logout" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log out</button>
+          <p v-if="isAuthenticated">Hello: {{ user?.name || user?.email }}</p>
+       <p class="px-4" v-if="!isAuthenticated">Va rog sa va logati ca sa accesati platforma</p>
+       <LoginBtn v-if="!isAuthenticated"/>
          </template>
        </Navbar>
 <div class="flex justify-center">
@@ -163,5 +164,8 @@
   </template>
   <script setup>
 import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink} from 'flowbite-vue';
+import LoginBtn from '@/components/LoginBtn.vue';
+import { useAuth0 } from '@auth0/auth0-vue';
+const {user, isAuthenticated}  = useAuth0();
   </script>
   
